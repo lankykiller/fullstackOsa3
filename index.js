633c215643c1e6560bdb03184f7480e00b50ad25
lankykiller <dani.mwai@gmail.com>
2024-10-31 21:04:11 +0200

    tehtävät3-3.4

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+let persons= [
+    { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": "1"
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": "2"
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": "3"
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": "4"
+      }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello!</h1>')
+  })
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const count  = persons.length
+    var currentdate = new Date();
+    response.send(`Phonebook has info for ${count} people <br> ${currentdate}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      console.log("error")
+      response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
+
+  
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
